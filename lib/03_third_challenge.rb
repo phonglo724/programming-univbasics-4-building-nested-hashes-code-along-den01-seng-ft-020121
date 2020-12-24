05d91abcc771624ce13c20e548808ef1548c4410
Phong Lo <phonglo724@gmail.com>
2020-12-24 00:54:04 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -12,7 +12,7 @@ def third_challenge
         :hero => {
           name: "Romeo",
           age: "15",
-          status: "Alive"
+          status: "alive"
         },
       :hero_friends => []
    },
@@ -28,7 +28,7 @@ def third_challenge
       :heroine => {
         name: "Juliet",
         age: "15",
-        status: "Alive"
+        status: "alive"
       },
       :heroine_friends => []
   }
